b16b0d3a517682a8d17d7124266358b75f2be990
Paul Hebst <p.hebst@intigoo.de>
2024-04-27 18:26:45 +0200

    Add mock table for reading products

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,13 +1,34 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
+  products = [
+    {
+      name: 'Product 1',
+      price: 10,
+      category: 'Electronics',
+      description: 'This is a product description',
+    },
+    {
+      name: 'Product 2',
+      price: 20,
+      category: 'Electronics',
+      description: 'This is a product description',
+    },
+    {
+      name: 'Product 3',
+      price: 30,
+      category: 'Electronics',
+      description: 'This is a product description',
+    },
+  ];
   title = 'frontend';
 }
