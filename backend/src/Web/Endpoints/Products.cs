d35772ad05f5d35a16d97c99e482a40a9ec9a410
Paul Hebst <p.hebst@intigoo.de>
2024-04-27 13:56:13 +0200

    Add delete product endpoint

--- backend/src/Web/Endpoints/Products.cs
+++ backend/src/Web/Endpoints/Products.cs
@@ -1,4 +1,5 @@
 using backend.Application.Products.Commands.CreateProduct;
+using backend.Application.Products.Commands.DeleteProduct;
 
 namespace backend.Web.Endpoints;
 
@@ -6,11 +7,19 @@ public class Products : EndpointGroupBase
 {
     public override void Map(WebApplication app)
     {
-        app.MapGroup(this).MapPost(CreateProduct);
+        app.MapGroup(this)
+            .MapPost(CreateProduct)
+            .MapDelete(DeleteProduct, "{id}");
     }
 
     public Task<int> CreateProduct(ISender sender, CreateProductCommand command)
     {
         return sender.Send(command);
+    }
+
+    public async Task<IResult> DeleteProduct(ISender sender, int id)
+    {
+        await sender.Send(new DeleteProductCommand(id));
+        return Results.NoContent();
     }
 }
