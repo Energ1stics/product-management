dc9d4c79d76c3f8abbbbcfd72630c35afbe282bc
Paul Hebst <p.hebst@intigoo.de>
2024-04-27 19:06:24 +0200

    Refactor create product to its own component (~4h mark)

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ReadProductsComponent } from './components/read-products/read-products.component';
+import { CreateProductComponent } from './components/create-product/create-product.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ReadProductsComponent],
+  imports: [RouterOutlet, ReadProductsComponent, CreateProductComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
