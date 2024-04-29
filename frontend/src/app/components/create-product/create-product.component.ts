a665caed9723077606dae278ef6d1fc2dcfa5141
Paul Hebst <p.hebst@intigoo.de>
2024-04-29 09:52:36 +0200

    Extract product create form into seperate component

--- frontend/src/app/components/create-product/create-product.component.ts
+++ frontend/src/app/components/create-product/create-product.component.ts
@@ -1,91 +1,33 @@
 import { Component } from '@angular/core';
-import {
-  FormBuilder,
-  FormControl,
-  FormGroup,
-  ReactiveFormsModule,
-  Validators,
-} from '@angular/forms';
 import { ProductService } from '../../services/product.service';
-import { CommonModule } from '@angular/common';
-import { CategoryService } from '../../services/category.service';
-import { Category } from '../../models/category';
+import { ProductFormComponent } from '../product-form/product-form.component';
+import { Product } from '../../models/product';
 
 @Component({
   selector: 'app-create-product',
   standalone: true,
-  imports: [ReactiveFormsModule, CommonModule],
+  imports: [ProductFormComponent],
   templateUrl: './create-product.component.html',
   styleUrl: './create-product.component.css',
 })
 export class CreateProductComponent {
-  constructor(
-    private formBuilder: FormBuilder,
-    private productService: ProductService,
-    private categoryService: CategoryService,
-  ) {}
-
-  categories: Category[] = [];
-
-  ngOnInit() {
-    this.categoryService.getCategories().subscribe((categories) => {
-      this.categories = categories;
-    });
-  }
+  constructor(private productService: ProductService) {}
 
   apiPending = false;
   apiError = false;
 
-  productForm = this.formBuilder.group({
-    name: new FormControl('', {
-      nonNullable: true,
-      validators: [
-        Validators.required,
-        Validators.minLength(3),
-        Validators.maxLength(50),
-      ],
-    }),
-    price: new FormControl(0, {
-      nonNullable: true,
-      validators: [
-        Validators.required,
-        Validators.min(1),
-        Validators.max(999_999_99),
-      ],
-    }),
-    category: new FormGroup({
-      id: new FormControl(-1, {
-        nonNullable: true,
-        validators: [Validators.required, Validators.min(0)],
-      }),
-    }),
-    description: new FormControl('', {
-      validators: [Validators.maxLength(500)],
-    }),
-  });
-
-  onSubmit() {
-    console.log(this.productForm.controls.price.errors);
-    if (this.productForm.invalid) {
-      this.productForm.markAllAsTouched();
-      return;
-    }
-
-    this.productForm.markAsUntouched();
+  onSubmit(product: Omit<Product, 'id'>) {
     this.apiPending = true;
 
-    this.productService
-      .createProduct(this.productForm.getRawValue())
-      .subscribe({
-        error: () => {
-          this.apiPending = false;
-          this.apiError = true;
-        },
-        complete: () => {
-          this.apiError = false;
-          this.apiPending = false;
-          this.productForm.reset();
-        },
-      });
+    this.productService.createProduct(product).subscribe({
+      error: () => {
+        this.apiPending = false;
+        this.apiError = true;
+      },
+      complete: () => {
+        this.apiError = false;
+        this.apiPending = false;
+      },
+    });
   }
 }
---
+++ frontend/src/app/components/product-form/product-form.component.ts
@@ -0,0 +1,80 @@
+import { CommonModule } from '@angular/common';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { Category } from '../../models/category';
+import { ProductService } from '../../services/product.service';
+import { CategoryService } from '../../services/category.service';
+import { Product } from '../../models/product';
+
+@Component({
+  selector: 'app-product-form',
+  standalone: true,
+  imports: [ReactiveFormsModule, CommonModule],
+  templateUrl: './product-form.component.html',
+  styleUrl: './product-form.component.css',
+})
+export class ProductFormComponent {
+  @Input() product?: Product;
+  @Input() apiPending = false;
+  @Input() apiError = false;
+  @Output() onSubmitEvent = new EventEmitter<Omit<Product, 'id'>>();
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private categoryService: CategoryService,
+  ) {}
+
+  categories: Category[] = [];
+
+  ngOnInit() {
+    this.categoryService.getCategories().subscribe((categories) => {
+      this.categories = categories;
+    });
+    if (this.product) {
+      this.productForm.patchValue(this.product);
+    }
+  }
+
+  productForm = this.formBuilder.group({
+    name: new FormControl('', {
+      nonNullable: true,
+      validators: [
+        Validators.required,
+        Validators.minLength(3),
+        Validators.maxLength(50),
+      ],
+    }),
+    price: new FormControl(0, {
+      nonNullable: true,
+      validators: [
+        Validators.required,
+        Validators.min(1),
+        Validators.max(999_999_99),
+      ],
+    }),
+    category: new FormGroup({
+      id: new FormControl(-1, {
+        nonNullable: true,
+        validators: [Validators.required, Validators.min(0)],
+      }),
+    }),
+    description: new FormControl('', {
+      validators: [Validators.maxLength(500)],
+    }),
+  });
+
+  onSubmit() {
+    if (this.productForm.invalid) {
+      this.productForm.markAllAsTouched();
+      return;
+    }
+
+    this.onSubmitEvent.emit(this.productForm.getRawValue());
+  }
+}
