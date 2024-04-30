4deb4d689dcab0f9af44e2d6a83dee0be9f7d069
Paul Hebst <p.hebst@intigoo.de>
2024-04-30 16:36:47 +0200

    Move header to seperate component

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -2,11 +2,17 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ReadProductsComponent } from './components/read-products/read-products.component';
 import { CreateProductComponent } from './components/create-product/create-product.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ReadProductsComponent, CreateProductComponent],
+  imports: [
+    RouterOutlet,
+    ReadProductsComponent,
+    CreateProductComponent,
+    HeaderComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
